d900d30227309c58a087caa5032392290fe8e557
hlibdyzyniuryk <hlib_dzyzniuryk@stucn.ua>
2025-09-18 21:08:29 +0200

    Додав main.py з усіма завданнями

---
+++ topic_01/task2.py
@@ -0,0 +1,7 @@
+s = "   hello world   "
+
+print("strip():", s.strip())          
+print("capitalize():", s.capitalize()) 
+print("title():", s.title())           
+print("upper():", s.upper())           
+print("lower():", s.lower())          
---
+++ topic_01/task3.py
@@ -0,0 +1,6 @@
+﻿def discriminant(a, b, c):
+    return b**2 - 4*a*c
+
+# приклад використання
+a, b, c = 1, -3, 2   # x² - 3x + 2 = 0
+print("Дискримінант:", discriminant(a, b, c))
